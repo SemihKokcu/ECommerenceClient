292c43ef185c4e68449a6f3ee45f8a6f23a5eed4
SemihKokcu <semihkkc1@gmail.com>
2023-01-24 00:21:59 +0300

    ui and admin arch designed with first commit

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './products/products.component';
+import { ProductsModule } from './products/products.module';
+import { HomeModule } from './home/home.module';
+import { BasketsModule } from './baskets/baskets.module';
 
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsModule,
+    HomeModule,
+    BasketsModule
   ]
 })
 export class ComponentsModule { }
