cf75bf1e1649ba7f2010d26a28275c9c8ec02cfe
SemihKokcu <semihkkc1@gmail.com>
2023-04-01 16:05:02 +0300

    siganlR added and tested with product on dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +13,17 @@ import { AlertifyService, MessageType, Position } from 'src/app/services/admin/a
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner:NgxSpinnerService,private alertify:AlertifyService
-  ) {
+  constructor(spinner:NgxSpinnerService,private alertify:AlertifyService,private singalRService:SignalRService) 
+  {
     super(spinner);
+    singalRService.start(HubUrls.ProductHub )
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this.singalRService.on(ReceiveFunctions.ProductAddedMessageRecieve,message=>{
+      this.alertify.message(message,{messageType:MessageType.Notify,position:Position.TopRight})
+    })
   }
   m(){
     this.alertify.message("message",{
